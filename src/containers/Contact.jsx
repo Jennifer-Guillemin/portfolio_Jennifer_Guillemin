4d5ce83f7df8a953a728457faf7c68c65a164450
Jenni <jennifer.1707@hotmail.fr>
2024-04-21 18:19:41 +0200

    version 1.2

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 
 function Contact() {
-  const contact = {
-    email: "jennifer.1707@hotmail.fr",
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("4e8e0916-389f-486f-92ba-8265294ff568", "YOUR_ACCESS_KEY_HERE");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+    }
   };
 
   return (
@@ -13,7 +32,7 @@ function Contact() {
         <p>
           N’hésitez pas à m’écrire ! Je vous répondrais en moins de 24 heures !
         </p>
-        <form action={`mailto:${contact.email}`} method="post">
+        <form onSubmit={onSubmit}>
           <label htmlFor="nom">Nom :</label>
           <input type="text" name="nom" id="nom" required />
           <label htmlFor="email">Email :</label>
